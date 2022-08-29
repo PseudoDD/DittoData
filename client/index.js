dfb611ac715b61b1bbec025d600ea1397a616fa0
“ian-ma-nyc” <sinma1995@gmail.com>
2022-08-29 18:54:11 -0400

    backend init merge dev

--- client/index.js
+++ client/index.js
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../client/app.jsx';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material";
-import App from "./App.jsx";
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
+import App from './App.jsx';
 
 render(
-  <App/>,
-  document.getElementById("root")//
+  <App />,
+  document.getElementById('root') //
 );
->>>>>>> dev
